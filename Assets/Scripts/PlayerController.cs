c49bf3a8485e08f7819c0454d2ca1a270152823c
Montrelld <MOntrelld@nycstudents.net>
2025-09-16 12:36:13 -0400

    Part 1.3 Complete

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,9 +11,12 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
+
+    public float speed = 20;
+
     void Update()
     {
         //Move the vehicle forward
-        transform.Translate(Vector3.forward * 20);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }
